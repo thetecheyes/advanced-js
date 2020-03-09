2ffa00512d419493536d484006fb89fc0f3def5a
Faisal Wahab <someone@nowhere.com>
2020-03-09 07:33:41 +0600

    double vs tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,21 @@
+// ! == checks only the values are similar or not.
+
+const one = 1;
+const two = true;
+
+if (one == two) {
+    console.log("True");
+} else {
+    console.log("False");
+}
+
+// ! === checks the values are similar or not with their Data Type.
+
+const first = 2;
+const second = '2';
+
+if (one === two) {
+    console.log("True");
+} else {
+    console.log("False");
+}
